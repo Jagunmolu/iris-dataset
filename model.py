b2f8e2a2fab7edbe9667010be96829459ac22b4a
Jagunmolu <kolawolejonathan@gmail.com>
2024-05-21 10:33:10 +0100

    created a basic template for the streamlit app

--- model.py
+++ model.py
@@ -1,23 +1,27 @@
 import pandas as pd
+import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 
+# random seed
+seed = 42
+
 # loading the data
-iris_df = pd.read_csv("./data/iris.csv")
-iris_df.sample(frac=1, random_state=42)
+iris_df = pd.read_csv("./data/Iris.csv")
+iris_df.sample(frac=1, random_state=seed)
 
 # selecting features and target data
 X = iris_df[["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]]
-y = iris_df[["Species"]]
+y = iris_df["Species"]
 
 # split data into train and test sets
 # 70% training and 30% test
 X_train, X_test, y_train, y_test = train_test_split(
-X, y, test_size=0.3, random_state=42, stratify=y)
+X, y, test_size=0.3, random_state=seed, stratify=y)
 
 # create an instance of the random forest classifier
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf = RandomForestClassifier(n_estimators=100, random_state=seed)
 
 # train the classifier on the training data
 clf.fit(X_train, y_train)
@@ -28,3 +32,5 @@ y_pred = clf.predict(X_test)
 # calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}") # Accuracy: 0.91
+
+joblib.dump(clf, "rf_model.sav")
